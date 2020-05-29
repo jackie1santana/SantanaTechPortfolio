a48950461259be52550f8d0e14d79c5f0bd3bcf4
Jackie Santana <jackiebrown.nyc@gmail.comgit config --global user.email jackiebrown.nyc@gmail.com>
2020-05-28 23:34:17 -0500

    deploy

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config({ path: "./config/.env" })
 
 const app = express()
 
-const port = process.env.PORT 
+const port = process.env.PORT || 4000
 
 app.use(cors())
 app.use(express.json())
