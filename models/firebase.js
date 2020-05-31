4920bccff3ed949e1f7ff15cd993dae8d53b55c1
Jackie Santana <jackiebrown.nyc@gmail.comgit config --global user.email jackiebrown.nyc@gmail.com>
2020-05-30 20:25:13 -0500

    trying to fix firebase database issue

--- models/firebase.js
+++ models/firebase.js
@@ -2,7 +2,10 @@ const admin = require("firebase-admin");
 const serviceAccount = require('./serviceAccount.json')
 
 const firebase = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount,{
+    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  }),
   databaseURL: "https://devportfolio-may-2020.firebaseio.com",
 });
 
