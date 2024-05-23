849d424f681d07cc61b82a151901d9a0b1520b53
jackie1santana <jackietristansantana@gmail.com>
2024-05-23 13:25:27 -0500

    fix port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const morgan = require('morgan')
 
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "./config/.env" });
